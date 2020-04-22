a0912ce2d865ecf8b4573cd450c6c0dc138c91cb
GuillemExe <guillem.arrojo@gmail.com>
2020-04-22 19:45:19 +0200

    Agregada los controllers de Projecte y Tasca. Agregado sistema de rutas en web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Api\Projecte;
+use Api\Tasca;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,40 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// TOKEN RETURN - OPTIONAL
+// Route::get('/projecte', 'ProjecteController@tokenReturn');
+
+
+// PROJECTE
+// INDEX
+Route::get('/projectes', 'ProjecteController@index')->name('projectes.index');
+// CREATE
+Route::get('/projectes/create', 'ProjecteController@create')->name('projectes.create');
+// STORE
+Route::post('/projectes', 'ProjecteController@store')->name('projectes.store');
+// SHOW
+Route::get('/projectes/{projecte}', 'ProjecteController@show')->name('projectes.show');
+// EDIT
+Route::get('/projectes/{projecte}/edit', 'ProjecteController@edit')->name('projectes.edit');
+// UPDATE
+Route::match(['put','patch'], '/projectes/{projecte}', 'ProjecteController@update')->name('projectes.update');
+// DESTROY
+Route::delete('/projectes/{projecte}', 'ProjecteController@destroy')->name('projectes.destroy');
+
+
+// PROJECTE - TASCA
+// INDEX
+Route::get('/projectes/{projecte}/tasques', 'TascaController@index')->name('projectes.tasques.index');
+// CREATE
+Route::get('/projectes/{projecte}/tasques/create', 'TascaController@create')->name('projectes.tasques.create');
+// STORE
+Route::post('/projectes/{projecte}/tasques', 'TascaController@store')->name('projectes.tasques.store');
+// SHOW
+Route::get('/projectes/{projecte}/tasques/{tasque}', 'TascaController@show')->name('projectes.tasques.show');
+// EDIT
+Route::get('/projectes/{projecte}/tasques/{tasque}/edit', 'TascaController@edit')->name('projectes.tasques.edit');
+// UPDATE
+Route::match(['put','patch'], '/projectes/{projecte}/tasques/{tasque}', 'TascaController@update')->name('projectes.tasques.update');
+// DESTROY
+Route::delete('/projectes/{projecte}/tasques/{tasque}', 'TascaController@destroy')->name('projectes.tasques.destroy');
